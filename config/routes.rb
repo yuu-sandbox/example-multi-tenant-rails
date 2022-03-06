454f81cf4829191e14bfc38fb9f7fcf29951927b
yuu <yuu@users.noreply.github.com>
2022-03-06 09:35:17 +0000

    rails subdomain required

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+class SubdomainRequired
+  def self.matches?(request)
+    subdomain = request.subdomains.first
+    # tenant = Author.find_by subdomain: subdomain
+    tenant = true
+    # subdomain.present? && tenant
+    subdomain.present? && /^[a-zA-Z][a-zA-Z0-9-]*{2}$/.match(subdomain)
+  end
+end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints(SubdomainRequired) do
+    get '/', to: "home#index"
+  end
+
 end
